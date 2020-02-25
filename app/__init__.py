51d6b9c8f42e8c54d302d05f39fcef89985a0a69
Alvaro Farias <afariasfermin@gmail.com>
2020-02-25 17:12:50 -0300

    :ok_hand: updated aplication file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restful import Api
 from config.application import config
 from app.http.controllers.api.PredictionController import PredictionController
@@ -9,6 +10,8 @@ app.config.from_mapping(
     DEBUG=config['debug'],
     SECRET_KEY=config['key']
 )
+
+CORS(app, origins=config['origins'])
 
 api = Api(app)
 api.add_resource(PredictionController, '/<string:data>', '/prediction/<string:data>')
